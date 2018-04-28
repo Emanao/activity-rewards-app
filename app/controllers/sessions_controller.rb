0fc51a6740da180fd184dbf8614eb7788dc6ea5b
github emanao <github emanao@me.com>
2018-04-28 12:45:05 -0400

    Add user's login feature

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,19 @@
 class SessionsController < ApplicationController
+    before_action :set_user, only: [:create]
   def new
+    @user = User.new
   end
 
   def create
+    if(@user && @user.authenticate(params[:user][:password]))
+        session[:user_id] = @user.user_id
+        redirect_to users_path
+    else
+        render :new
+    end
+  end
+  private
+  def set_user
+    @user = User.find_by(params[:user][:name])
   end
 end
