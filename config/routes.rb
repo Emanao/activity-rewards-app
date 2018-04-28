e584c17b31e672371db0891bfd2b9ceab7948be1
github emanao <github emanao@me.com>
2018-04-28 09:56:40 -0400

    Add session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html    
   root "static_pages#home"
   get "/signup" => "users#new"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post '/users' => "users#create"
+  get "/login" => "sessions#new"
+  post "/sessions" => "sessions#create"
+  get "/logout" => "sessions#logout"
 end
