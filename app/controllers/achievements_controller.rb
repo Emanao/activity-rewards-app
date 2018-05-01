a69033dded12c59e1755bb723697a824d23b4aa5
github emanao <github emanao@me.com>
2018-05-01 13:14:09 -0400

    Show users achievements

--- app/controllers/achievements_controller.rb
+++ app/controllers/achievements_controller.rb
@@ -1,2 +1,20 @@
 class AchievementsController < ApplicationController
+    def index
+        if(params[:user_id])
+            @user = User.find_by(id: params[:user_id])
+            @achievements = @user.achievements
+        else
+        end
+    end
+    def update
+        raise params.inspect
+    end
+
+    private
+    def achievement_params
+        params.require(:user).permit(:activity_ids)
+    end
+    def user_params
+        params.require(:user_id)
+    end
 end
