0dbe7fc42faa9ac937a2a27614bbb94f2b0b48e4
github emanao <github emanao@me.com>
2018-05-01 13:08:37 -0400

    Add user nested routes for activities and achievements. Add admin activities routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html    
+  #root
   root "static_pages#home"
+  #signup
   get "/signup" => "users#new"
   post '/users' => "users#create"
-  resources :users, only: [:show] do
-    resources :activities, only: [:show, :index]
-  end
-  resources :activities, only: [:new, :create, :edit, :update]
+  #login
   get "/login" => "sessions#new"
   post "/sessions" => "sessions#create"
   get "/logout" => "sessions#logout"
+  #github login
   get '/auth/:provider/callback' => 'sessions#create'
 
+  #nested resources
+  resources :users, only: [:show] do
+    resources :activities, only: [:index, :show]
+    resources :achievements, only: [:index, :update]
+  end 
+  
+  resources :activities, only: [:new, :create, :edit, :update]
 end
