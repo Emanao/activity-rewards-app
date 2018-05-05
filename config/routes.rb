c54ba692228b7b20ac8678ad7997a0bbc65f7b00
github emanao <github emanao@me.com>
2018-05-05 06:59:07 -0400

    Delete some comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,16 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html    
   #root
   root "static_pages#home"
+  
   #signup
   get "/signup" => "users#new"
   post '/users' => "users#create"
+  
   #login
   get "/login" => "sessions#new"
   post "/sessions" => "sessions#create"
   get "/logout" => "sessions#logout"
+  
   #github login
   get '/auth/:provider/callback' => 'sessions#create'
 
@@ -17,7 +20,8 @@ Rails.application.routes.draw do
     resources :activities, only: [:index, :update]
     resources :achievements, only: [:index, :update]
   end 
-  #resources :admins, only: [:index]
-  resources :activities, only: [:index]
-  resources :users, only: [ :index, :update]
+
+  #Admin resources
+  resources :activities, only: [:index, :edit, :update]
+  resources :users, only: [ :index, :edit, :update]
 end
